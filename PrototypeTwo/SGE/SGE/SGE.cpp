4b096f8b490e6acf3b8d8139b5164653bda3fa46
shession <SHession@users.noreply.github.com>
2015-02-04 14:01:14 +0000

    Adds library for second prototype

---
+++ PrototypeTwo/SGE/SGE/SGE.cpp
@@ -0,0 +1,30 @@
+// This is the main DLL file.
+
+#include "stdafx.h"
+
+#include "SGE.h"
+
+void SGEFramework::Game::Run(){
+	
+
+}
+
+void SGEFramework::Game::Initalize(){
+
+
+}
+
+void SGEFramework::Game::Draw(){
+
+
+}
+
+void SGEFramework::Game::Update(){
+
+
+}
+
+void SGEFramework::Game::LoadContent(){
+
+
+}
