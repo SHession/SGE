f84eb35af156c78835ae6ad007b7c448f2f9d8bc
shession <shession@5cd7c475-3489-442c-a87c-8b6eeb2eac10>
2015-02-19 14:22:53 +0000

    SGE now creates a window and DirectX context on Game.Run

--- trunk/PrototypeTwo/SGE/SGE/SGE.cpp
+++ trunk/PrototypeTwo/SGE/SGE/SGE.cpp
@@ -6,19 +6,37 @@
 
 using namespace SGEFramework;
 
-Game::Game(){
-	puts("Hello");
+//Forward Declerations
+HRESULT CompileShader( _In_ LPCWSTR srcFile, _In_ LPCSTR entryPoint, _In_ LPCSTR profile, _Outptr_ ID3DBlob** blob );
+LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam );
 
+
+Game::Game(){
 }
 
 Game::~Game(){
-	puts("Goodbye");
-
+	CleanUp();
 }
 
 void Game::Run(HINSTANCE hInstance, int nCmdShow){
 	InitWindow(hInstance, nCmdShow);
 	InitDevice();
+
+	MSG msg = {0};
+    while( WM_QUIT != msg.message )
+    {
+        if( PeekMessage( &msg, NULL, 0, 0, PM_REMOVE ) )
+        {
+           TranslateMessage( &msg );
+           DispatchMessage( &msg );		   
+        }
+        else
+        {
+		//Render the application
+			Update();
+			Draw();
+        }
+    }
 }
 
 
@@ -27,7 +45,7 @@ HRESULT Game::InitWindow( HINSTANCE hInstance, int nCmdShow ){
 	WNDCLASSEX wcex;
 	wcex.cbSize = sizeof( WNDCLASSEX );
 	wcex.style = CS_OWNDC;
-	//wcex.lpfnWndProc = WndProc;
+	wcex.lpfnWndProc = WndProc;
 	wcex.cbClsExtra = 0;
     wcex.cbWndExtra = 0;
 	wcex.hInstance = hInstance;
@@ -56,6 +74,130 @@ HRESULT Game::InitWindow( HINSTANCE hInstance, int nCmdShow ){
 }
 
 HRESULT Game::InitDevice(){
+	//Create a rect for the viewport based on the window size
+	RECT rc;
+	GetClientRect( mainWnd, &rc );
+    UINT width = rc.right - rc.left;
+    UINT height = rc.bottom - rc.top;
+
+	UINT createDeviceFlags = 0;
+	#ifdef _DEBUG
+    createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+	#endif
+
+	//Describe swap chain
+	DXGI_SWAP_CHAIN_DESC sd;
+	sd.BufferDesc.Width = width;
+	sd.BufferDesc.Height = height;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.BufferCount =1;
+	sd.OutputWindow = mainWnd;
+	sd.Windowed = true;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	sd.Flags = 0;
+
+
+	//create swap chain/ context and view
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(0, D3D_DRIVER_TYPE_HARDWARE, 0, createDeviceFlags, 0, 0,D3D11_SDK_VERSION, &sd, &g_pSwapChain
+												,&g_pd3dDevice, &g_featureLevel, &g_pImmediateContext);
+
+	if(FAILED(hr)) return hr;
+
+
+	//Add a render target view to the swap chain
+	ID3D11Texture2D *BackBuffer;
+	hr = g_pSwapChain->GetBuffer(0, _uuidof(ID3D11Texture2D), (LPVOID*) &BackBuffer);
+	if(FAILED(hr)) return hr;
+	hr = g_pd3dDevice->CreateRenderTargetView(BackBuffer,0,&g_pRenderTargetView);
+	if(FAILED(hr)) return hr;
+
+	//Add a depth stencil texture
+	D3D11_TEXTURE2D_DESC descDepth;
+	ZeroMemory(&descDepth, sizeof(descDepth));
+	descDepth.Width = width;
+    descDepth.Height = height;
+    descDepth.MipLevels = 1;
+    descDepth.ArraySize = 1;
+    descDepth.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+    descDepth.SampleDesc.Count = 1;
+    descDepth.SampleDesc.Quality = 0;
+    descDepth.Usage = D3D11_USAGE_DEFAULT;
+    descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+    descDepth.CPUAccessFlags = 0;
+    descDepth.MiscFlags = 0;
+
+    hr = g_pd3dDevice->CreateTexture2D( &descDepth, NULL, &g_pDepthStencil );
+    if( FAILED( hr ) ) return hr;
+
+	//Create depth stencil view 
+	D3D11_DEPTH_STENCIL_VIEW_DESC descDSV;
+	ZeroMemory( &descDSV, sizeof(descDSV) );
+    descDSV.Format = descDepth.Format;
+    descDSV.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+    descDSV.Texture2D.MipSlice = 0;
+
+	hr = g_pd3dDevice->CreateDepthStencilView( g_pDepthStencil, &descDSV, &g_pDepthStencilView );
+    if( FAILED( hr ) ) return hr;
+
+	//Output merger stage
+	g_pImmediateContext->OMSetRenderTargets( 1, &g_pRenderTargetView, g_pDepthStencilView );
+
+	D3D11_VIEWPORT vp;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	vp.Width = width;
+	vp.Height = height;
+	vp.MinDepth = 0;
+	vp.MaxDepth = 1;
+
+	//Bind an array of viewports to the rasterizer stage of the pipeline.
+	g_pImmediateContext->RSSetViewports(1,&vp);
+
+	//Compile the vertex shader
+	ID3DBlob *pVSBlob = NULL;
+	hr = CompileShader( L"VertexShader.hlsl", "VS", "vs_4_0", &pVSBlob );
+	if(FAILED(hr)) return hr;
+
+	//Create the vertex shader
+	hr = g_pd3dDevice->CreateVertexShader( pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), NULL, &g_pVertexShader );
+	if(FAILED(hr)) return hr;
+
+	//Define the input layout
+	D3D11_INPUT_ELEMENT_DESC layout[] =
+	 {
+        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "NORMAL", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12,D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 24,D3D11_INPUT_PER_VERTEX_DATA, 0 },
+    };
+	UINT numElements = ARRAYSIZE( layout );
+
+	//Create the input layout
+	hr = g_pd3dDevice->CreateInputLayout( layout, numElements, pVSBlob->GetBufferPointer(),
+                                          pVSBlob->GetBufferSize(), &g_pVertexLayout );
+	pVSBlob->Release();
+	if(FAILED(hr)) return hr;
+
+	//Set the input layout
+	g_pImmediateContext->IASetInputLayout(g_pVertexLayout);
+
+	//Compile the pixel shader
+	ID3DBlob *pPSBlob = NULL;
+	hr = CompileShader( L"PixelShader.hlsl", "PS", "ps_4_0", &pPSBlob );
+	if(FAILED(hr)) return hr;
+
+	//Create the pixel shader
+	hr = g_pd3dDevice->CreatePixelShader( pPSBlob->GetBufferPointer(), pPSBlob->GetBufferSize(), NULL, &g_pPixelShader);
+	if(FAILED(hr)) return hr;
+
 
 	return S_OK;
 }
@@ -66,16 +208,99 @@ void Game::Initalize(){
 }
 
 void Game::Draw(){
-
-
+	float ClearColor[4] = { 0.1f, 0.1f, 0.5f, 1.0f }; //red,green,blue,alpha
+    g_pImmediateContext->ClearRenderTargetView( g_pRenderTargetView, ClearColor );
+	g_pSwapChain->Present( 0, 0 );
 }
 
 void Game::Update(){
-
-
+	puts("Running");
 }
+
 
 void Game::LoadContent(){
 
 
+}
+
+void Game::CleanUp(){
+	if( g_pImmediateContext ) g_pImmediateContext->ClearState();
+    if( g_pVertexBuffer ) g_pVertexBuffer->Release();
+    if( g_pVertexLayout ) g_pVertexLayout->Release();
+    if( g_pVertexShader ) g_pVertexShader->Release();
+    if( g_pPixelShader ) g_pPixelShader->Release();
+    if( g_pRenderTargetView ) g_pRenderTargetView->Release();
+    if( g_pSwapChain ) g_pSwapChain->Release();
+    if( g_pImmediateContext ) g_pImmediateContext->Release();
+    if( g_pd3dDevice ) g_pd3dDevice->Release();
+	if(g_pDepthStencil) g_pDepthStencil->Release();
+	if(g_pDepthStencilView) g_pDepthStencilView->Release();
+
+
+}
+
+//Load a shader from a file
+HRESULT CompileShader( _In_ LPCWSTR srcFile, _In_ LPCSTR entryPoint, _In_ LPCSTR profile, _Outptr_ ID3DBlob** blob )
+{
+    if ( !srcFile || !entryPoint || !profile || !blob )
+       return E_INVALIDARG;
+
+    *blob = nullptr;
+
+    UINT flags = D3DCOMPILE_ENABLE_STRICTNESS;
+#if defined( DEBUG ) || defined( _DEBUG )
+    flags |= D3DCOMPILE_DEBUG;
+#endif
+
+    const D3D_SHADER_MACRO defines[] = 
+    {
+        "EXAMPLE_DEFINE", "1",
+        NULL, NULL
+    };
+
+    ID3DBlob* shaderBlob = nullptr;
+    ID3DBlob* errorBlob = nullptr;
+    HRESULT hr = D3DCompileFromFile( srcFile, defines, D3D_COMPILE_STANDARD_FILE_INCLUDE,
+                                     entryPoint, profile,
+                                     flags, 0, &shaderBlob, &errorBlob );
+    if ( FAILED(hr) )
+    {
+        if ( errorBlob )
+        {
+            OutputDebugStringA( (char*)errorBlob->GetBufferPointer() );
+            errorBlob->Release();
+        }
+
+        if ( shaderBlob )
+           shaderBlob->Release();
+
+        return hr;
+    }    
+
+    *blob = shaderBlob;
+
+    return hr;
+}
+
+LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam )
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+    switch( message )
+    {
+        case WM_PAINT:
+            hdc = BeginPaint( hWnd, &ps );
+            EndPaint( hWnd, &ps );
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage( 0 );
+            break;
+
+        default:
+            return DefWindowProc( hWnd, message, wParam, lParam );
+    }
+
+    return 0;
 }
