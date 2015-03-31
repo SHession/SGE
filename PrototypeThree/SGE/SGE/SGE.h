37a7cdaebe3f152a4b44f3cf7df2ffeabbe8e22b
shession <SHession@users.noreply.github.com>
2015-03-31 12:42:05 +0000

    Adds the prototype three solution

---
+++ PrototypeThree/SGE/SGE/SGE.h
@@ -0,0 +1,43 @@
+// SGE.h
+
+#pragma once
+
+
+namespace SGE {
+	namespace Framework {
+		class Game{
+
+
+		};
+
+		class GameObject{
+
+
+		};
+
+		class GameTime{
+
+
+		};
+	}
+
+	namespace Graphics {
+		class GraphicDevice{
+
+
+
+		};
+	}
+
+	namespace Sound{
+		class SoundDevice{
+
+
+		};
+	}
+
+	namespace Input{
+
+
+	}
+}
