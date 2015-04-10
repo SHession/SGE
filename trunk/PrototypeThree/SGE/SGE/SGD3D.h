b508debe463dd21b31e9e5da84fdd88d49c32fd6
shession <shession@5cd7c475-3489-442c-a87c-8b6eeb2eac10>
2015-04-10 15:13:14 +0000

    Fixes the bug preventing Release builds from working. Adds the ability to load texture, Adds a ProcessContent method which is called after load content and allows the graphics device to process the users content. Adds various other parts to the DirectXDevice leading closer to being able to render meshes.

--- trunk/PrototypeThree/SGE/SGE/SGD3D.h
+++ trunk/PrototypeThree/SGE/SGE/SGD3D.h
@@ -6,11 +6,15 @@
 #include <D3Dcompiler.h>
 #include <DirectXMath.h>
 #include <DirectXPackedVector.h>
+#include <wincodec.h>
+#include <dxgiformat.h>
+#include <assert.h>
 
 #include <vector>
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <memory>
 
 namespace SGD3D {
 
@@ -29,6 +33,11 @@ namespace SGD3D {
 		UINT startIndex;
 	};
 
+	struct CB_VS_PER_OBJECT{
+		DirectX::XMMATRIX gWorld;
+		DirectX::XMMATRIX gWorldViewProj;
+	};
+
 	void DestroyMesh(Mesh *mesh);
 
 	class DirectXDevice : public SGE::Graphics::GraphicDevice{
@@ -36,16 +45,17 @@ namespace SGD3D {
 			__declspec( dllexport ) DirectXDevice();
 			__declspec( dllexport ) ~DirectXDevice();
 
-			HRESULT InitializeDevice(HWND hWnd);
-			HRESULT Draw();
-			HRESULT DrawMesh(SGE::Graphics::Mesh);
-			HRESULT DrawGameObject(SGE::Framework::GameObject);
-			HRESULT Clear();
+			 HRESULT InitializeDevice(HWND hWnd);
+			 HRESULT ProcessContent();
+			 HRESULT Draw();
+			 HRESULT DrawMesh(SGE::Graphics::Mesh);
+			 HRESULT DrawGameObject(SGE::Framework::GameObject);
+			 HRESULT Clear();
 
-			HRESULT LoadObj(wchar_t * filename, SGE::Graphics::Mesh* mesh);				
-		    HRESULT LoadTexture(wchar_t* filename, SGE::Graphics::Texture* texture);
-			HRESULT LoadVShader(wchar_t* filename, char* entryPoint, SGE::Graphics::VertexShader *shader);
-			HRESULT LoadPShader(wchar_t* filename, char* entryPoint, SGE::Graphics::PixelShader *shader);
+			 HRESULT LoadObj(wchar_t * filename, SGE::Graphics::Mesh* mesh);				
+		     HRESULT LoadTexture(wchar_t* filename, SGE::Graphics::Texture* texture);
+			 HRESULT LoadVShader(wchar_t* filename, char* entryPoint, SGE::Graphics::VertexShader *shader);
+			 HRESULT LoadPShader(wchar_t* filename, char* entryPoint, SGE::Graphics::PixelShader *shader);
 
 		private:
 			D3D_DRIVER_TYPE         driverType;
@@ -56,6 +66,13 @@ namespace SGD3D {
 			ID3D11RenderTargetView* renderTargetView;
 			ID3D11Texture2D*		depthStencil;
 			ID3D11DepthStencilView*	depthStencilView;
+			ID3D11InputLayout*      inputLayout;
+			ID3D11Buffer*           vertexBuffer;
+			ID3D11Buffer*           indexBuffer;
+			ID3D11Buffer*           constantBuffer;
+			ID3D11SamplerState*     samplerLinear;
+			IWICImagingFactory*		imagingFactory;
+			IWICBitmapDecoder*		bitmapDecoder;
 
 			std::vector<ID3D11VertexShader*> vertexShaders;
 			std::vector<ID3D11PixelShader*> pixelShaders;
@@ -64,6 +81,8 @@ namespace SGD3D {
 
 			HRESULT CleanUp();
 			HRESULT CompileShader(LPCWSTR srcFile,LPCSTR entryPoint, LPCSTR profile, ID3DBlob** blob );
+			DXGI_FORMAT _WICToDXGI( const GUID& guid );
+			size_t _WICBitsPerPixel( REFGUID targetGuid , IWICImagingFactory *factory);
 
 	};
 
